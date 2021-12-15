3ac4ab2d952a3334352915f609c4aecf2b8e55c7
Jozdien <jozdien@gmail.com>
2021-12-16 00:49:20 +0530

    Created pipeline from audio file to annotated transcript.  Added functions to format transcript from diarize.py and pass words through entropy calculator.

--- gpt2.py
+++ gpt2.py
@@ -5,7 +5,38 @@ tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
 model = GPT2LMHeadModel.from_pretrained('gpt2')
 
 
-def surprise(string, next_word):
+def get_entropy(string, next_word):
+	inputs = tokenizer(string, return_tensors="pt")
+	outputs = model(**inputs, labels=inputs["input_ids"])
+
+	logits = outputs.logits
+
+	next_token = tokenizer.encode(next_word)
+
+	return torch.mean(logits[0][-1][next_token]).item()
+
+
+def get_entropy_full(string, next_word):
+	inputs = tokenizer(string, return_tensors="pt")
+	outputs = model(**inputs, labels=inputs["input_ids"])
+
+	logits = outputs.logits
+
+	next_token = tokenizer.encode(next_word)
+
+	return logits[0][-1][next_token]
+
+
+def most_prob_cont(string):
+	inputs = tokenizer(string, return_tensors="pt")
+	outputs = model(**inputs, labels=inputs["input_ids"])
+
+	logits = outputs.logits
+
+	return tokenizer.decode(torch.argmax(logits[0][-1])), torch.max(logits[0][-1])
+
+
+def get_prob(string, next_word):
 	inputs = tokenizer(string, return_tensors="pt")
 	outputs = model(**inputs, labels=inputs["input_ids"])
 
@@ -14,4 +45,4 @@ def surprise(string, next_word):
 
 	next_token = tokenizer.encode(next_word)
 
-	return logits[0][-1][next_token].item()
+	return torch.mean(probs[0][-1][next_token]).item()
--- program.py
+++ program.py
@@ -1,24 +1,64 @@
 from diarize import transcript
-from gpt2 import surprise
+from gpt2 import get_entropy
 
 import pprint
 pp = pprint.PrettyPrinter(indent=4)
 
-transcript = transcript(AUDIO_FILENAME="audio.wav", num_speakers=2)
-for text in transcript:
-	start = text.index(":", text.find("speaker")) + 2
-	print(text[start:])
-pp.pprint(transcript)
+
+def transcript_sets(transcript):
+	ret = []
+
+	for line in transcript:
+		name_start = line.index("]") + 2
+		name_end = line.index(":", line.find("speaker"))
+
+		text_start = name_end + 2
+
+		name = line[name_start:name_end]
+		text = line[text_start:].split()
+
+		line_set = (name, text)
+
+		ret.append(line_set)
+
+	return ret
 
 
-'''
+def transcript_entropies(transcript):
+	running_prefix = ""
+	ret = []
 
-TODO:
+	for line_set in transcript:
+		name, text = line_set
+		running_prefix += name + ":"
 
-Write program file that takes in transcript as input and returns a list of sets of the form (speaker_name, text)
+		ret_set = (name, [])
 
-Write function that takes a set as input and calculates entropy for each word.
-	- For this, keep a running prefix string containing the entire conversation so far, which will be the first parameter to surprise()
-	- Everytime a new set is taken, append "speaker_name: " to the running prefix string and keep going
+		for word in text:
+			word_formatted = " " + word
+			word_surprise = get_entropy(running_prefix, word_formatted)
 
-'''
+			word_set = (word, word_surprise)
+
+			ret_set[1].append(word_set)
+
+			running_prefix += word_formatted
+
+		ret.append(ret_set)
+		running_prefix += "\n"
+
+	return ret
+
+
+
+if __name__ == '__main__':
+
+	transcript = transcript(AUDIO_FILENAME="audio.wav", num_speakers=2)
+
+	transcript_formatted = transcript_sets(transcript)
+	transcript_annotated = transcript_entropies(transcript_formatted)
+
+	with open("annotated_transcript.txt", "w+") as file:
+		for (name, text) in transcript_annotated:
+			file.write(f"{name}: {text}")
+			file.write("\n")
